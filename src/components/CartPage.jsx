4bb1e9ba20395958753fc0a9523cbdc841738e89
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-17 07:19:50 +1100

    feat: add removeItem function

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,7 +1,21 @@
+import { useOutletContext } from "react-router"
+import CartItem from "./CartItem"
+
 export default function CartPage() {
+    const { cart, setCart} = useOutletContext()
+
+    function removeItem(id) {
+        setCart(prev => prev.filter(item => item.id !== id))
+    }
+
     return (
         <>
             <h1>Cart Page</h1>
+            <div className="products-container">
+                {cart.map(item => (
+                    <CartItem key={item} item={item} removeItem={removeItem}/>
+                ))}
+            </div>
         </>
     )
 }
